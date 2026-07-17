fdfa0d1b954a6b35ecfa6ee54db3de65f9e2717e
sachinsoni27 <sachinsoniofficial2003@gmail.com>
2026-07-17 11:55:43 +0530

    01-variables.js

---
+++ Basics-0/01-variables.js
@@ -0,0 +1,18 @@
+const accountId = 1001
+let accountEmail = "sachin@gmail.com"
+var  accountPassword = "sachin@123"
+var accountCity = "Orai"
+let accountState;
+
+
+
+//accountId = 1002
+console.log(accountId);
+accountEmail ="shivang@gmail.com"
+console.log(accountEmail)
+accountPassword = "shivang@123"
+console.log(accountPassword)
+// so let ,var values can be changed but not const value 
+//var is not used now days beacuse of issue in block scope and function scope
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
