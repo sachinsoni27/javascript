aaa82c22e0ced29353b3bdf57d0b67c258ce428d
sachinsoni27 <sachinsoniofficial2003@gmail.com>
2026-07-18 12:38:31 +0530

    04-comparision

---
+++ Basics-0/04-comparision.js
@@ -0,0 +1,32 @@
+ console.log(2>1);//true
+  console.log(2>=1);//true
+   console.log(2<1);//false
+    console.log(2<=1);//false
+     console.log(2==1);//false
+      console.log(2!=1);//true
+
+
+       console.log("    ");
+       console.log("    ");
+       console.log("    ");
+       console.log("    ");
+
+       console.log(null> 0);//false
+       console.log(null>= 0);//true
+       console.log(null< 0);//false
+       console.log(null<= 0);//true
+       console.log(null== 0);//false
+       console.log(null!= 0);//true
+
+       console.log("    ");
+       console.log("    ");
+       console.log("    ");
+       console.log("    ");
+
+
+       console.log(undefined> 0);//false
+       console.log(undefined>= 0);//false
+       console.log(undefined< 0);//false
+       console.log(undefined<= 0);//false
+       console.log(undefined== 0);//false
+       console.log(undefined!= 0);//true
