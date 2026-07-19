03d933a75d27e80e83ab17eec13bb6ab4b7f84a4
sachinsoni27 <sachinsoniofficial2003@gmail.com>
2026-07-20 00:48:42 +0530

    stack and heap

--- Basics-0/stackAndHeap.js
+++ Basics-0/stackAndHeap.js
@@ -1,5 +1,5 @@
-//stack
-//heap  
+//stack is used for storing primitive data types like numbers, strings, booleans, null, undefined, and symbols. These are stored in a fixed size and are accessed directly from the stack memory.
+//heap   is used for storing reference data types like objects, arrays, and functions. These are stored in a dynamic size and are accessed through references from the stack memory.
 
 
 let userOne = {
