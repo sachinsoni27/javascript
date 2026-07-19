e19cfcdae5e4b285324b9e429487e491bde80ecf
sachinsoni27 <sachinsoniofficial2003@gmail.com>
2026-07-20 00:46:42 +0530

    stack and heap

---
+++ Basics-0/stackAndHeap.js
@@ -0,0 +1,15 @@
+//stack
+//heap  
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "hitesh@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
